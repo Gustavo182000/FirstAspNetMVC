8e74ea86d82810edbce25a7831f7aa22ddfd4c43
Gustavo Braga <gb95131aq@gmail.com>
2023-09-13 14:53:35 -0300

    Criado Model para Emprestimos

---
+++ FirstAspNetMVC/Models/EmprestimosModel.cs
@@ -0,0 +1,11 @@
+﻿namespace FirstAspNetMVC.Models
+{
+    public class EmprestimosModel
+    {
+        public int Id { get; set; }
+        public string? Recebedor { get; set; }
+        public string? Fornecedor { get; set; }
+        public string? Titulo { get; set; }
+        public DateTime DataAtualizacao { get; set; } = DateTime.Now;
+    }
+}
