5cb3da7de36a683f49d78547a35abbdb34c6691c
Gustavo Braga <gb95131aq@gmail.com>
2023-09-19 11:09:35 -0300

    Criado controller e view para emprestimos

---
+++ FirstAspNetMVC/Controllers/EmprestimoController.cs
@@ -0,0 +1,21 @@
+﻿using FirstAspNetMVC.Data;
+using FirstAspNetMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstAspNetMVC.Controllers
+{
+    public class EmprestimoController : Controller
+    {
+        readonly private ApplicationDbContext _context;
+        public EmprestimoController(ApplicationDbContext _context)
+        {
+            this._context = _context;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<EmprestimosModel> emprestimos = _context.Emprestimos;
+
+            return View(emprestimos);
+        }
+    }
+}
